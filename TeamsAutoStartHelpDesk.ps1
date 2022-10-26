cdfd77b044835093cecf6346ad12eb2412e0f7dc
Marek <marekwinicjuszkapusta@gmail.com>
2022-10-26 12:01:43 +0200

    Create TeamsAutoStartHelpDesk.ps1

---
+++ TeamsAutoStartHelpDesk.ps1
@@ -0,0 +1,158 @@
+#First part of the script
+#Source: https://learn.microsoft.com/en-us/microsoftteams/scripts/powershell-script-teams-reset-autostart
+
+#Function to generate a log file
+if ((Test-Path -Path "$ENV:SystemDrive\support\logs\apps" -PathType Container) -ne $true ) {mkdir "$ENV:SystemDrive\support\logs\apps" | Out-Null}
+$LogFile = "$ENV:SystemDrive\support\logs\apps\TeamsAutoStart.log"
+
+Function LogWrite{
+    Param ([string]$logstring)
+    Add-content $Logfile -value "$(Get-Timestamp) $logstring"
+}
+
+<#
+.SYNOPSIS
+This script allows you to reset all autostart settings to the default settings for Teams.
+.DESCRIPTION
+If you want to use the "Prevent Microsoft Teams from starting automatically after installation"
+Group Policy setting, make sure you first set the Group Policy setting to the value you want 
+before you run this script.
+#>
+
+LogWrite "** STARTING Microsoft Teams Auto Start Off Helpdesk edition, if there are any issues, get in touch with Marek.Kapusta@fujitsu.com. **"
+write-host "** STARTING Microsoft Teams Auto Start Off Helpdesk edition, if there are any issues, get in touch with Marek.Kapusta@fujitsu.com. **"
+logwrite "Script started by: $env:UserName on $env:ComputerName"
+
+$ErrorActionPreference = "Stop"
+
+$TeamsDesktopConfigJsonPath = [System.IO.Path]::Combine($env:APPDATA, 'Microsoft', 'Teams', 'desktop-config.json')
+
+$TeamsUpdatePath = [System.IO.Path]::Combine($env:LOCALAPPDATA, 'Microsoft', 'Teams', 'Update.exe')
+
+Function Test-RegistryValue {
+    param(
+        [Alias("PSPath")]
+        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [String]$Path
+        ,
+        [Parameter(Position = 1, Mandatory = $true)]
+        [String]$Name
+    ) 
+
+    process {
+        if (Test-Path $Path) {
+            $Key = Get-Item -LiteralPath $Path
+            if ($null -ne $Key.GetValue($Name, $null)) {
+                $true
+            } else {
+                $false
+            }
+        } else {
+            $false
+        }
+    }
+}
+
+Function Test-Remove-RegistryValue {
+    param (
+        [Alias("PSPath")]
+        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [String]$Path
+        ,
+        [Parameter(Position = 1, Mandatory = $true)]
+        [String]$Name
+    )
+
+    process {
+        if (Test-RegistryValue -Path $Path -Name $Name) {
+            Write-Host "Removing registry key $Path\$Name"
+            Remove-ItemProperty -Path $Path -Name $Name
+        }
+    }
+}
+
+# when determining whether Teams should be auto-started we are checking three flags
+Write-Host "Removing Auto-Start-related artifacts"
+
+# 0. Close Teams, if running
+$teamsProc = Get-Process -name Teams -ErrorAction SilentlyContinue
+if ($null -ne $teamsProc) {
+    Write-Host  "Stopping Microsoft Teams..."
+    Stop-Process -Name Teams -Force
+    # wait some time
+    Start-Sleep 5
+} else {
+    Write-Host  "No running Teams process found"
+}
+
+# 1. Check that Teams process isn't still running
+$teamsProc = Get-Process -name Teams -ErrorAction SilentlyContinue
+
+if($null -eq $teamsProc) {
+    # 2. remove HKEY_CURRENT_USER\Software\Microsoft\Office\Teams\LoggedInOnce registry key
+    Test-Remove-RegistryValue -Path "HKCU:\Software\Microsoft\Office\Teams" -Name "LoggedInOnce"
+
+    # 3. remove HKEY_CURRENT_USER\Software\Microsoft\Office\Teams\HomeUserUpn registry key
+    Test-Remove-RegistryValue -Path "HKCU:\Software\Microsoft\Office\Teams" -Name "HomeUserUpn"
+
+    # 4. remove HKEY_CURRENT_USER\Software\Microsoft\Office\Teams\DeadEnd registry key
+    Test-Remove-RegistryValue -Path "HKCU:\Software\Microsoft\Office\Teams" -Name "DeadEnd"
+
+    # 5. remove HKCU:\Software\Microsoft\Office\Outlook\Addins\TeamsAddin.FastConnect registry key
+    Remove-Item -Path "HKCU:\Software\Microsoft\Office\Outlook\Addins\TeamsAddin.FastConnect" -ErrorAction SilentlyContinue
+
+    # 6. restore HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run\com.squirrel.Teams.Teams
+    if (!(Test-RegistryValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "com.squirrel.Teams.Teams")) {
+        Write-Host "Restoring registry key HKCU\Software\Microsoft\Windows\CurrentVersion\Run\com.squirrel.Teams.Teams"
+        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "com.squirrel.Teams.Teams" -Value "$TeamsUpdatePath --processStart ""Teams.exe"" --process-start-args ""--system-initiated"""
+    }
+
+    # 7. We are checking whether there are entries 'isLoggedOut' and 'openAtLogin' in the desktop-config.json file
+    if (Test-Path -Path $TeamsDesktopConfigJsonPath) {
+        Write-Host "Changing entries 'guestTenantId', 'isLoggedOut' and 'openAtLogin' in the desktop-config.json, if exist"
+
+        # open desktop-config.json file
+        $desktopConfigFile = Get-Content -path $TeamsDesktopConfigJsonPath -Raw | ConvertFrom-Json
+        $desktopConfigFile.PSObject.Properties.Remove("guestTenantId")
+        $desktopConfigFile.PSObject.Properties.Remove("isLoggedOut")
+        try {
+            $desktopConfigFile.appPreferenceSettings.openAtLogin = $true
+        } catch {
+            Write-Host  "openAtLogin JSON element doesn't exist"
+        }
+        $desktopConfigFile | ConvertTo-Json -Compress | Set-Content -Path $TeamsDesktopConfigJsonPath -Force
+    }
+} else {
+    Write-Host  "Teams process is still running, aborting script execution"
+}
+
+#-------------------------------------------------------------------------------------------------------------
+#Second part of the script
+#Source: http://woshub.com/disable-teams-autostart/
+#changing $teamsConfigFile to detect which user is logged in to find correct path
+
+$entry = $null -eq (Get-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Run)."com.squirrel.Teams.Teams"
+if ( !$entry ) {
+Remove-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Run -Name "com.squirrel.Teams.Teams"
+}
+$ActiveExplorer = (Get-WMIObject -query "SELECT * FROM win32_Process WHERE Name ='explorer.exe'" | Foreach { $owner = $_.GetOwner(); $_ | Add-Member -MemberType "Noteproperty" -name "Owner" -value $("{0}\{1}" -f $owner.Domain, $owner.User) -passthru }).Owner
+foreach($user in $ActiveExplorer){
+    $username = $user.Split('\')[1]
+    write-host "found $username as an active user"
+    $teamsConfigFile = "C:\Users\$username\AppData\Roaming\Microsoft\Teams\desktop-config.json"
+    $teamsConfig = Get-Content $teamsConfigFile -Raw
+    if ( $teamsConfig -match "openAtLogin`":false") {
+    break
+    }
+    elseif ( $teamsConfig -match "openAtLogin`":true" ) {
+    $teamsConfig = $teamsConfig -replace "`"openAtLogin`":true","`"openAtLogin`":false"
+    }
+    else {
+    $teamsAutoStart = ",`"appPreferenceSettings`":{`"openAtLogin`":false}}"
+    $teamsConfig = $teamsConfig -replace "}$",$teamsAutoStart
+    }
+    $teamsConfig | Set-Content $teamsConfigFile
+}
+
+LogWrite "** End of the Microsoft Teams Auto Start Off Helpdesk edition. **"
+write-host "** End of the Microsoft Teams Auto Start Off Helpdesk edition. **"
