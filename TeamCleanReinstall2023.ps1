d92383c79a84eba2944974ca81239c684e08c5d6
Marek <marekwinicjuszkapusta@gmail.com>
2023-03-23 13:52:50 +0100

    Create TeamCleanReinstall2023.ps1

---
+++ TeamCleanReinstall2023.ps1
@@ -0,0 +1,108 @@
+Write-host "Script for Clean reinstall of Microsoft Teams. Created by Marek.Kapusta@fujitsu.com"
+#last update 23.03.2023
+#adding function to restart device after script 05.10.2022
+
+$ErrorActionPreference = 'SilentlyContinue'
+#Function to generate a timestamp that is added to the log file
+function Get-TimeStamp {
+    return "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)   
+}
+    
+#Function to generate a log file
+if ((Test-Path -Path "$ENV:SystemDrive\support\logs\apps" -PathType Container) -ne $true ) {mkdir "$ENV:SystemDrive\support\logs\apps" | Out-Null}
+$LogFile = "$ENV:SystemDrive\support\logs\apps\CleanTeamsRemoval.log"
+
+Function LogWrite{
+    Param ([string]$logstring)
+    Add-content $Logfile -value "$(Get-Timestamp) $logstring"
+    Write-Host $logstring
+}
+
+function removeLeftOvers{
+    [CmdletBinding()]
+    Param(
+    [String]$Path
+    )
+    if(Test-Path $Path){
+        LogWrite "found $path, clearing content of the folder"
+        Get-ChildItem $path | Remove-Item -Recurse -Confirm:$false
+        remove-item $Path -Recurse -Confirm:$false
+        if(Test-Path $Path){
+            LogWrite "error: complications with deleting $path, will try again after 10 seconds"
+            $attemp = 2
+            while((Test-Path $Path)-and($attemp -le 10)){
+            Start-Sleep -Seconds 10
+            LogWrite "$attemp attempt to delete $path"
+            Get-ChildItem $path | Remove-Item -Recurse -Confirm:$false
+            remove-item $Path -Recurse -Confirm:$false
+            $attemp++
+            }
+            if(Test-Path $Path){
+                LogWrite "error: unable to delete $path"
+            }
+            else{
+                LogWrite "$path successfully deleted"
+            }
+        }
+        else{
+            LogWrite "$path successfully deleted"
+        }
+    }
+    else{
+        LogWrite "unable to find $path"
+    }
+}
+
+LogWrite "** STARTING Clean Teams Removal Script **"
+#checking if Teams Wide Installer is installed
+#if Teams Wide installer is not installed, teams won't reinstall after device is restarted, leaving user without MS Teams
+if(test-path "C:\Program Files (x86)\Teams Installer\Teams.exe"){
+    tskill outlook
+    $ActiveExplorer = (Get-WMIObject -query "SELECT * FROM win32_Process WHERE Name ='explorer.exe'" | Foreach { $owner = $_.GetOwner(); $_ | Add-Member -MemberType "Noteproperty" -name "Owner" -value $("{0}\{1}" -f $owner.Domain, $owner.User) -passthru }).Owner
+    foreach($user in $ActiveExplorer){
+        $username = $user.Split('\')[1]
+        if (Test-Path "$($ENV:SystemDrive)\Users\$username\AppData\Local\Microsoft\Teams\update.exe") { 
+                try {
+                    LogWrite "Teams folder with update.exe found for user $username, uninstalling MS Teams..."
+                    Start-Process -FilePath "$($ENV:SystemDrive)\Users\$username\AppData\Local\Microsoft\Teams\Update.exe" -ArgumentList "-uninstall -s" -EA Stop            
+                    removeLeftOvers -path "C:\Users\$username\AppData\Roaming\Microsoft\teams"
+                    removeLeftOvers -path "C:\Users\$username\AppData\Roaming\teams"
+                    removeLeftOvers -path "C:\Users\$username\AppData\local\Microsoft\teams"
+                    removeLeftOvers -path "C:\Users\$username\AppData\local\Microsoft\teamsMeetingAddin"
+                    removeLeftOvers -path "C:\Users\$username\AppData\local\Microsoft\teamsPresenceAddin"
+                    removeLeftOvers -path "C:\Users\$username\AppData\local\Microsoft\SquirrelTemp"
+                }
+                Catch { 
+                LogWrite "Teams app Uninstall for user $username Failed! Error Message:"
+                LogWrite $_.Exception.Message
+                Out-Null
+                }
+            }
+        elseif(Test-Path "C:\Program Files (x86)\Microsoft\Teams\update.exe"){
+            LogWrite "Teams folder with update.exe found for user in C:\Program Files (x86)\Microsoft\Teams\, uninstalling MS Teams..."
+            Start-Process -FilePath "C:\Program Files (x86)\Microsoft\Teams\update.exe" -ArgumentList "-uninstall -s" -EA Stop
+            removeLeftOvers -path  "C:\Program Files (x86)\Microsoft\Teams"
+            removeLeftOvers -path "C:\Program Files (x86)\Microsoft\teamsMeetingAddin"
+            removeLeftOvers -path "C:\Program Files (x86)\Microsoft\teamsPresenceAddin"
+            removeLeftOvers -path "C:\Users\$username\AppData\Roaming\Microsoft\teams"
+            removeLeftOvers -path "C:\Users\$username\AppData\Roaming\teams"
+            removeLeftOvers -path "C:\Users\$username\AppData\local\Microsoft\teams"
+            removeLeftOvers -path "C:\Users\$username\AppData\local\Microsoft\teamsMeetingAddin"
+            removeLeftOvers -path "C:\Users\$username\AppData\local\Microsoft\teamsPresenceAddin"
+            removeLeftOvers -path "C:\Users\$username\AppData\local\Microsoft\SquirrelTemp"
+            
+        }
+        else{
+            LogWrite "MS Teams for $username not found"
+        }
+    }
+    
+    LogWrite "** ENDING Clean Teams Removal Script **"
+    #restart device
+    #shutdown -r -t 0
+}#End of the script with installed Windows wide installer
+else{
+    LogWrite "** Warning! MS team wide installer is not installed **"
+    LogWrite "Before running script, please install MS Teams machine wide installer x64"
+}
+read-host "Press enter to close the script"
